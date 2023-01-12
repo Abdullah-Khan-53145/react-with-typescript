38bb33f0e5910af96ff0187780614e0957621e42
Abdullah Khan <abdullah.khan53145@gmail.com>
2023-01-12 04:24:54 -0800

    basic of props using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import "./App.css";
+import Greet from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
-  return <div className="App"></div>;
+  const personName = {
+    first: "Abdullah",
+    last: "Khan",
+  };
+  const nameList = [
+    {
+      first: "Abdullah",
+      last: "Khan",
+    },
+    {
+      first: "Rana",
+      last: "Sanaullah",
+    },
+    {
+      first: "Hammad",
+      last: "Haider",
+    },
+  ];
+  return (
+    <div className="App">
+      <Greet name="Abdullah" messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Greet.tsx
@@ -0,0 +1,18 @@
+type GreetProps = {
+  name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
+};
+function Greet(props: GreetProps) {
+  return (
+    <div>
+      <h2>
+        {props.isLoggedIn
+          ? "Welcome {props.name}, you have {props.messageCount} unread messages"
+          : "Welcome guest"}
+      </h2>
+    </div>
+  );
+}
+
+export default Greet;
