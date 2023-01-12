cd38ea0c4aa719e9946af9f02f31b1d9e3fcce37
Abdullah Khan <abdullah.khan53145@gmail.com>
2023-01-12 04:59:02 -0800

    Event types in props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import Greet from "./components/Greet";
-import { Heading } from "./components/Heading";
-import { Status } from "./components/Status";
+
 
 function App() {
   return (
     <div className="App">
-      <Status status="error" />
-      <Heading childern="hello" />
-      <Greet name="Abdullah" isLoggedIn={true} />
+    
     </div>
   );
 }
